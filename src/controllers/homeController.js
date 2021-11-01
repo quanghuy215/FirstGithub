a3100b71b85cf963091624e06bc5e4f4b739c00b
Harry Nguyen <nguyenquanghuy2021@gmail.com>
2021-11-01 15:12:22 +0700

    first commit

---
+++ src/controllers/homeController.js
@@ -0,0 +1,33 @@
+import db from '../models/index'
+import CRUDService from "../services/CRUDService"
+let getHomePage = async (req,res) =>{
+    try {
+        let data = await db.User.findAll();
+        console.log('-------------------')
+        console.log(data)
+        return res.render("homepage.ejs", {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+       console.log(e); 
+    }  
+}
+
+let getAboutPage = (req,res) =>{
+    return res.render("about.ejs");
+}
+
+let getCRUD = (req,res) =>{
+    return res.render("crud.ejs");
+}
+let postCRUD = async (req,res) =>{
+    let message = await CRUDService.createNewUser(req.body)
+    console.log(message);
+    return res.send("post crud from server");
+}
+module.exports = {
+    getHomePage :getHomePage,
+    getAboutPage : getAboutPage,
+    getCRUD : getCRUD,
+    postCRUD : postCRUD,
+}
